a66d181d7c50e0de5a235bf9efe49aba26363f97
Fernando Herrera <fernando.herrera85@gmail.com>
2020-09-02 14:27:49 -0400

    Fin sección 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      
     });
   }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+
+  ngOnInit() {
+
+    let numero = 10;
+    // debugger;
+
+    numero = 20;
+
+    numero = numero + 10;
+
+    numero = 0;
+
+    console.log( numero );
+  }
 
 }
